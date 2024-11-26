a7a22260aa6dee289411b87278dd394aa71fca95
MuhammadSabir132 <165038409+MuhammadSabir132@users.noreply.github.com>
2024-11-26 11:13:43 +0500

    Update childcomponent.tsx

--- src/app/components/childcomponent.tsx
+++ src/app/components/childcomponent.tsx
@@ -2,7 +2,7 @@ const ChildComponent = (props: any) => {
     console.log(props)
     return (
         <div className="justify-center  items-center   bg-gray-500">
-            <h1 className="text-3xl justify-center  items-center">
+            <h1 className="text-5xl justify-center  items-center">
               {props.name} 
               {props.fvtdish}
               {props.fvtcolor}
